251381e91e9522127099fbf78154377116ac87af
Jasonjiang27 <yaozu.jiang@mathartsys.com>
2018-09-30 13:54:35 +0800

    day07

--- www/coroweb.py
+++ www/coroweb.py
@@ -53,7 +53,8 @@ def get_named_kw_args(fn):
     params = inspect.signature(fn).parameters
     for name, param in params.items():
         if param.kind == inspect.Parameter.KEYWORD_ONLY:
-            return True
+            args.append(name)
+        return tuple(args)
 
 def has_named_kw_args(fn):
     params = inspect.signature(fn).parameters
@@ -96,18 +97,18 @@ class RequestHandler(object):
         if self._has_var_kw_arg or self._has_named_kw_args or self._required_kw_args:
             if request.method == 'POST':
                 if not request.content_type:
-                    return web.HTTPBadRequest('Missing Content-Type.')
+                    return web.HTTPBadRequest()
                 ct = request.content_type.lower()
                 if ct.startwith('application/json'):
                     params = await request.json()
                     if not isinstance(params, dict):
-                        return web.HTTPBadRequest('JSON body must be object.')
+                        return web.HTTPBadRequest()
                     kw = params
                 elif ct.startwith('application/x-www-form-urlencoded') or ct.startwith('multipart/form-data'):
                     params = await request.post()
                     kw = dict(**params)
                 else:
-                    return web.HTTPBadRequest('Unsupported Content-type: %s' % request.content_type)
+                    return web.HTTPBadRequest()
             if request.method == 'GET':
                 qs = request.query_string
                 if qs:
@@ -136,7 +137,7 @@ class RequestHandler(object):
         if self._required_kw_args:
             for name in self._required_kw_args:
                 if not name in kw:
-                    return web.HTTPBadRequest('Missing argument: %s' % name)
+                    return web.HTTPBadRequest()
         logging.info('call with args: %s' % str(kw))
         try:
             r = await self._func(**kw)
@@ -144,34 +145,34 @@ class RequestHandler(object):
         except APIError as e:
             return dict(error=e.error, data=e.data, message=e.message)
 
-    def add_static(self, app):
-        path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
-        app.router.add_static('/static/', path)
-        logging.info('add static %s => %s' % ('/static/', path))
+def add_static(app):
+    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
+    app.router.add_static('/static/', path)
+    logging.info('add static %s => %s' % ('/static/', path))
 
-    def add_route(self, app, fn):
-        method = getattr(fn, '__method__', None)
-        path = getattr(fn, '__route__', None)
-        if path is None or method is None:
-            raise ValueError('@get or @post not defined in %s.' % str(fn))
-        if not asyncio.iscoroutinefunction(fn) and not inspect.isgenerator(fn):
-            fn = asyncio.coroutine(fn)
-        logging.info('add route %s %s => %s(%s)' % (method, path, fn.__name__, ','.join(inspect.signature(fn).parameters.keys())))
-        app.router.add_route(method, path, RequestHandler(app, fn))
-    
-    def add_routes(self, app, module_name):
-        n = module_name.rfind('.')
-        if n == (-1):
-            mod = __import__(module_name[:n], globals(), locals())
-        else:
-            name = module_name[n+1:]
-            mod = getattr(__import__(module_name[:n], globals(), locals(), [name]), name)
-        for attr in dir(mod):
-            if attr in dir(mod):
-                continue
-            fn = getattr(mod, attr)
-            if callable(fn):
-                method = getattr(fn, '__method__', None)
-                path = getattr(fn, '__route__', None)
-                if method and path:
-                    add_route(app, fn)
+def add_route(app, fn):
+    method = getattr(fn, '__method__', None)
+    path = getattr(fn, '__route__', None)
+    if path is None or method is None:
+        raise ValueError('@get or @post not defined in %s.' % str(fn))
+    if not asyncio.iscoroutinefunction(fn) and not inspect.isgenerator(fn):
+        fn = asyncio.coroutine(fn)
+    logging.info('add route %s %s => %s(%s)' % (method, path, fn.__name__, ','.join(inspect.signature(fn).parameters.keys())))
+    app.router.add_route(method, path, RequestHandler(app, fn))
+
+def add_routes(app, module_name):
+    n = module_name.rfind('.')
+    if n == (-1):
+        mod = __import__(module_name[:n], globals(), locals())
+    else:
+        name = module_name[n+1:]
+        mod = getattr(__import__(module_name[:n], globals(), locals(), [name]), name)
+    for attr in dir(mod):
+        if attr in dir(mod):
+            continue
+        fn = getattr(mod, attr)
+        if callable(fn):
+            method = getattr(fn, '__method__', None)
+            path = getattr(fn, '__route__', None)
+            if method and path:
+                add_route(app, fn)
